f8872150be69ce8be622772bf1ba0d01da3cafa5
xodidsmlek <xodidsmlek@gmail.com>
2021-12-16 02:57:53 +0900

    top_menu

--- _site/assets/js/default.js
+++ _site/assets/js/default.js
@@ -1,4 +1,13 @@
 window.onload = function(){
+    // 현재 스크롤 값
+    let currentScrollValue = document.documentElement.scrollTop;
+
+    if(currentScrollValue > 200){
+        top_menu.style.position         = 'fixed';
+        top_menu.style.top              = '0px';
+        top_menu.style.backgroundColor  = '#fcfcfc';
+        top_menu.style.boxShadow        = '0px 0px 5px #e3e3e3 ';
+    }
 
     top_menu.addEventListener('mouseover', function(event){
         if (event.preventDefault) event.preventDefault();
@@ -11,7 +20,15 @@ window.onload = function(){
         if (event.preventDefault) event.preventDefault();
         else event.cancelBubble = true;
 
-        top_menu_mouseout();
+        top_menu_mouseout(currentScrollValue);
+    });
+
+    document.addEventListener('scroll', function(event) {
+        if (event.preventDefault) event.preventDefault();
+        else event.cancelBubble = true;
+        currentScrollValue = document.documentElement.scrollTop;
+
+        top_menu_fixed(currentScrollValue);
     });
 }
 
@@ -21,8 +38,25 @@ function top_menu_mouseover(height){
     top_menu.style.boxShadow            = '0px 0px 5px #e3e3e3 ';
 }
 
-function top_menu_mouseout(){
+function top_menu_mouseout(scrollVal){
+    if(scrollVal <= 200){
+        top_menu.style.backgroundColor  = '#fff';
+        top_menu.style.boxShadow        = 'none';
+    }
     top_menu.style.height               = '50px';
-    top_menu.style.backgroundColor      = '#fff';
-    top_menu.style.boxShadow            = 'none';
+    
 }
+
+function top_menu_fixed(scrollVal){
+    if(scrollVal > 200){
+        top_menu.style.position         = 'fixed';
+        top_menu.style.top              = '0px';
+        top_menu.style.backgroundColor  = '#fcfcfc';
+        top_menu.style.boxShadow        = '0px 0px 5px #e3e3e3 ';
+    } else {
+        top_menu.style.position         = 'relative';
+        top_menu.style.backgroundColor  = '#fff';
+        top_menu.style.boxShadow        = 'none';
+    }
+}
+
